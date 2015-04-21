93bace86aace5e0c34d6cd8a6449f475c7de9ad1
Ivan Pushkin <imetalguardi@gmail.com>
2015-04-21 12:53:39 +0300

    update migrations

---
+++ migrations/m150421_093238_create_meta_tag_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use yii\db\Schema;
+
+/**
+ * Class m150421_093238_create_meta_tag_table migration
+ */
+class m150421_093238_create_meta_tag_table extends \yii\db\Migration
+{
+    /**
+     * migration table name
+     */
+    public $tableName = '{{%meta_tag}}';
+
+    /**
+     * @inheritdoc
+     */
+    public function safeUp()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+
+        $this->createTable(
+            $this->tableName,
+            [
+                'id' => Schema::TYPE_PK,
+
+                'name' => Schema::TYPE_STRING . ' NOT NULL COMMENT "Specifies a name for the metadata"',
+                'http_equiv' => Schema::TYPE_STRING . ' NOT NULL COMMENT "Provides an HTTP header for the information/value of the content attribute"',
+                'default_value' => Schema::TYPE_TEXT . ' DEFAULT NULL COMMENT "Default value for the meta tag"',
+                'description' => Schema::TYPE_STRING . ' NOT NULL COMMENT "Short description for tag"',
+                'is_active' => Schema::TYPE_SMALLINT . '(1) UNSIGNED NOT NULL DEFAULT 1 COMMENT "Register or not this tag on the front"',
+                'position' => Schema::TYPE_INTEGER . ' UNSIGNED NOT NULL DEFAULT 0',
+            ],
+            $tableOptions
+        );
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown()
+    {
+        $this->dropTable($this->tableName);
+    }
+}
---
+++ migrations/m150421_093740_create_meta_tag_table.php
@@ -0,0 +1,59 @@
+<?php
+
+use yii\db\Schema;
+
+/**
+ * Class m150421_093740_create_meta_tag_table migration
+ */
+class m150421_093740_create_meta_tag_table extends \yii\db\Migration
+{
+    /**
+     * migration table name
+     */
+    public $tableName = '{{%meta_tag_content}}';
+
+    /**
+     * @inheritdoc
+     */
+    public function safeUp()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+
+        $this->createTable(
+            $this->tableName,
+            [
+                'model_name' => Schema::TYPE_STRING . ' NOT NULL COMMENT "Model name"',
+                'model_id' => Schema::TYPE_INTEGER . ' UNSIGNED NOT NULL COMMENT "Model ID"',
+                'language' => Schema::TYPE_STRING . '(16) NOT NULL COMMENT "Language"',
+
+                'meta_tag_id' => Schema::TYPE_INTEGER . ' NOT NULL COMMENT "Meta tag"',
+                'meta_tag_content' => Schema::TYPE_TEXT . ' DEFAULT NULL COMMENT "Meta tag content"',
+            ],
+            $tableOptions
+        );
+
+        $this->addPrimaryKey('', $this->tableName, ['model_name', 'model_id', 'language', 'meta_tag_id']);
+        $this->createIndex('index-model_name-model_id-language', $this->tableName, ['model_name', 'model_id', 'language']);
+
+        $this->addForeignKey(
+            'fk-meta_tag_content-meta_tag_id-meta_tag-id',
+            $this->tableName,
+            'meta_tag_id',
+            '{{%meta_tag}}',
+            'id',
+            'CASCADE',
+            'RESTRICT'
+        );
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown()
+    {
+        $this->dropTable($this->tableName);
+    }
+}
